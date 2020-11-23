f1c9ac9811a4b01e5ced63d822034737d7a71f56
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-23 22:42:50 +0100

    Creating acceptants test for index page and buttons for user to choose

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return ( 
+      <div>
+      <header><h1>Play RPS</h1></header>
+      <p>Choose your weapon:</p>
+      <button id="rock">Rock</button>
+      <button id="paper">Paper</button>
+      <button id="scissor">Scissor</button>
+      </div>
+    )
   }
 }
 
