4556af6b0894fb5f2d3af74c0000cdf03dbbf70d
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-26 22:36:02 +0100

    Fixed the computer logic

--- src/Components/PlayButtons.jsx
+++ src/Components/PlayButtons.jsx
@@ -8,22 +8,33 @@ class PlayButtons extends Component {
   };
 
   rockHandler = () => {
-    this.setState({ weaponRock: "You choose rock!", weaponPaper: "" });
+    this.setState({
+      weaponRock: "You choose rock!",
+      weaponPaper: "",
+      weaponScissor: "",
+    });
   };
 
   paperHandler = () => {
-    this.setState({ weaponPaper: "You choose paper!" });
+    this.setState({
+      weaponPaper: "You choose paper!",
+      weaponRock: "",
+      weaponScissor: "",
+    });
   };
 
   scissorHandler = () => {
-    this.setState({ weaponScissor: "You choose scissor!" });
+    this.setState({
+      weaponScissor: "You choose scissor!",
+      weaponRock: "",
+      weaponPaper: "",
+    });
   };
 
   render() {
     const { weaponRock, weaponPaper, weaponScissor } = this.state;
 
-    // Spel-logiken
-    const gameLogic = function () {
+    let gameLogic = function () {
       const random = Math.random();
       if (random < 0.35) {
         return console.log("rock");
@@ -36,13 +47,31 @@ class PlayButtons extends Component {
 
     return (
       <div>
-        <button id="rock" onClick={gameLogic}>
+        <button
+          id="rock"
+          onClick={() => {
+            gameLogic();
+            this.rockHandler();
+          }}
+        >
           Rock
         </button>
-        <button id="paper" onClick={this.paperHandler}>
+        <button
+          id="paper"
+          onClick={() => {
+            gameLogic();
+            this.paperHandler();
+          }}
+        >
           Paper
         </button>
-        <button id="scissor" onClick={this.scissorHandler}>
+        <button
+          id="scissor"
+          onClick={() => {
+            gameLogic();
+            this.scissorHandler();
+          }}
+        >
           Scissor
         </button>
         <div>
